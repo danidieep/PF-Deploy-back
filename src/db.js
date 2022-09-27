edce9cd461d5de3312f6be9fec9bf0378f2e642f
Pedr0calvo <100429639+Pedr0calvo@users.noreply.github.com>
2022-09-27 12:15:21 -0300

    Update db.js

--- src/db.js
+++ src/db.js
@@ -3,6 +3,8 @@ const fs = require("fs");
 const path = require("path");
 require("dotenv").config();
 
+const { DATABASE_URL } = process.env;
+
 const sequelize = new Sequelize(DATABASE_URL, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
