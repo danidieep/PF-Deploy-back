20ee8be026eaa998a4b1182bac4cceba33812b79
Pedr0calvo <100429639+Pedr0calvo@users.noreply.github.com>
2022-09-29 19:16:52 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ server.use(cookieParser());
 server.use(morgan("dev"));
 server.use(cors())
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", CORS_URL); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", '*'); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
@@ -28,7 +28,7 @@ server.use((req, res, next) => {
   next();
 });
 
-server.use('/', routes);
+server.use('https://artket-pf-deploy.herokuapp.com', routes);
 
 // Error catching endware.
 server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
